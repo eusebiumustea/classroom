7827ae1af54573eb60d786ada6c1340eaf4f751a
eusebiumustea <musteaeusebiu14@gmail.com>
2025-02-11 21:50:19 +0200

    initial commit

---
+++ src/models/auth/types.ts
@@ -0,0 +1,16 @@
+export interface Register {
+  firstname: string;
+  lastname: string;
+  username: string;
+  email: string;
+  dateOfBirth: Date;
+  password: string;
+}
+export interface Login {
+  firstname: string;
+  lastname: string;
+  username: string;
+  email: string;
+  dateOfBirth: Date;
+  password: string;
+}
